8afbe8f7c84f7788846e4ac36ce370418c02c6a3
Ncmocha <nickichenbattle@gmail.com>
2017-12-20 17:18:26 -0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$(function(){
+  var duration = 500;
+  $("button:nth-child(-n+4)").on('mouseover',function(){
+    $(this).stop(true).animate({
+      backgroundColor:'#ae64ea',
+      color:'#fff'
+    },duration);
+  });
+  $("button:nth-child(-n+4)").on('mouseout',function(){
+    $(this).stop(true).animate({
+      backgroundColor:'#fff',
+      color:'#ffa68d'
+    },duration);
+  });
+  $("button:nth-child(n+5):nth-child(-n+8)").on('mouseover',function(){
+    $(this).stop(true).animate({
+      borderWidth:'8px',
+      color:'#968782'
+    },duration);
+  })
+  .on('mouseout',function(){
+    $(this).stop(true).animate({
+      borderWidth:'0px',
+      color:'#ffa68d'
+    },duration);
+  });
+ $("#wrapper button:nth-child(n+9)").on('mouseover',function(){
+   $(this).find('>span').stop(true).animate({
+     width:'100%'
+   },duration,'easeOutCirc');
+ })
+ .on('mouseout',function(){
+   $(this).find('>span').stop(true).animate({
+     width:'0%'
+   },duration,'easeOutCirc');
+ });
+
+});
